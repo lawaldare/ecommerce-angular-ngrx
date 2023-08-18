d0960866172f97c5dd4094cf31a4fecdf853fff9
Dare Lawal <lawaldare@gmail.com>
2023-08-18 10:13:04 +0100

    added total to the cart page

--- src/app/product/components/cart/cart.component.ts
+++ src/app/product/components/cart/cart.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { DashboardSelectors } from '../../state/product.selectors';
 import { Product } from '../../services/product.service';
+import { tap } from 'rxjs';
 
 @Component({
   selector: 'dl-cart',
@@ -12,7 +13,23 @@ import { Product } from '../../services/product.service';
   ],
 })
 export class CartComponent implements OnInit {
-  cartItemProducts$ = this.store.select(DashboardSelectors.cartItemProducts);
+  cartItemProducts$ = this.store
+    .select(DashboardSelectors.cartItemProducts)
+    .pipe(
+      tap((cartItems) => {
+        this.totalAmount = cartItems?.reduce((totalAmount, item) => {
+          const itemTotal = item.quantity * item.price;
+          totalAmount = totalAmount + itemTotal;
+          return totalAmount;
+        }, 0);
+      })
+    );
+
+  numberOfCartItems$ = this.store.select(
+    DashboardSelectors.numberOfItemsInCart
+  );
+
+  totalAmount: number | undefined;
 
   counts = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
